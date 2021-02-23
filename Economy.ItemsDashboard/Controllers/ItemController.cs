3ace77672765d89ec78c2de3656062ddc4811bb6
nailuj29gaming <54189157+nailuj29gaming@users.noreply.github.com>
2021-02-23 13:07:44 -0500

    forgot to comit all files

---
+++ Economy.ItemsDashboard/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Economy.ItemsDashboard.Models;
+using Economy.ItemsDashboard.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Economy.ItemsDashboard.Controllers {
+    public class ItemController : Controller {
+        private readonly ItemService _itemService;
+        
+        public ItemController(ItemService svc) {
+            _itemService = svc;
+        }
+        
+        [AllowAnonymous]
+        public ActionResult<IList<Item>> Index() => View(_itemService.Get());
+
+        [HttpGet]
+        public ActionResult Create() => View();
+
+        [HttpPost]
+        public ActionResult<Item> Create([FromForm] Item item) {
+            if (ModelState.IsValid) {
+                _itemService.Create(item);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult<Item> Edit(string id) => View(_itemService.Get(id));
+
+        [HttpPost]
+        public ActionResult Edit(Item item) {
+            if (ModelState.IsValid) {
+                _itemService.Update(item);
+                return RedirectToAction("Index");
+            }
+
+            return View("Submission");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string id) {
+            _itemService.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Economy.ItemsDashboard/Services/ItemService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Economy.ItemsDashboard.Models;
+using MongoDB.Driver;
+
+namespace Economy.ItemsDashboard.Services {
+    public class ItemService {
+        private readonly IMongoCollection<Item> _items;
+
+        public ItemService(IDatabaseSettings settings) {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _items = database.GetCollection<Item>("Items");
+        }
+
+        public Item Create(Item item) {
+            _items.InsertOne(item);
+            return item;
+        }
+
+        public List<Item> Get() =>
+            _items.Find(_ => true).ToList();
+
+        public Item Get(string id) =>
+            _items.Find(item => item.Id == id).SingleOrDefault();
+
+        public Item Search(string query) =>
+            _items.Find(item => item.Name.Contains(query)).SingleOrDefault();
+
+        public void Update(Item item) =>
+            _items.ReplaceOne(old => old.Id == item.Id, item);
+
+        public void Delete(string id) =>
+            _items.DeleteOne(item => item.Id == id);
+    }
+}
